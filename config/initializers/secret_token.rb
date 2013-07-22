d47ed8cb33df2f0e1d2063714022caa3d8ba7e47
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-07-22 21:34:11 +1200

    Initial project generation
    rails 4.0.0 skip AR skip TestUnit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Burgundy::Application.config.secret_key_base = '8691aafb349d71986ca43b459022508bcdfeef3eb7ca899eeb2a6d55539cdc88bf2ae01ea885b3896f00b00ed32ebee730685467054563620b240d352879f278'
